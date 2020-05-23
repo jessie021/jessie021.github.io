c7f0d0b529ec2482fc6771da8389abd6746c2633
jdetc021 <jdetc021@uottawa.ca>
2020-05-23 17:30:31 -0400

    cx

--- SEG3525-Module2/scripts/groceries.js
+++ SEG3525-Module2/scripts/groceries.js
@@ -97,17 +97,16 @@ function checked(){
 
 function restrictListProducts(prods, restriction) {
 let product_names = [];
-let product_sorted_with_price = [];
 
 for (let i=0; i<prods.length; i+=1) {
   if ((restriction == "Vegetarian") && (prods[i].vegetarian == true) ){
 
     if (globalOrganicTrue == true){
       if ((prods[i].organic == true)){
-        product_names.push(prods[i]);
+        product_names.push(prods[i].name+ prods[i].price);
       }
     }else{
-      product_names.push(prods[i]);
+      product_names.push(prods[i].name+ prods[i].price);
     }
 
   }
@@ -115,28 +114,28 @@ for (let i=0; i<prods.length; i+=1) {
 
     if (globalOrganicTrue == true){
       if ((prods[i].organic == true)){
-        product_names.push(prods[i]);
+        product_names.push(prods[i].name+  " " + prods[i].price);
       }
     }else{
-      product_names.push(prods[i]);
+      product_names.push(prods[i].name +  " " + prods[i].price);
     }
   }else if ((restriction == "GlutenFree&Vegetarian") && (prods[i].vegetarian == true)  && (prods[i].glutenFree == true) ){
     if (globalOrganicTrue == true){
       if ((prods[i].organic == true)){
-        product_names.push(prods[i]);
+        product_names.push(prods[i].name +  " " + prods[i].price);
       }
     }else{
-      product_names.push(prods[i]);
+      product_names.push(prods[i].name + " " + prods[i].price);
     }
   }
   else if (restriction == "None" ){
 
     if (globalOrganicTrue == true){
       if ((prods[i].organic == true)){
-        product_names.push(prods[i]);
+        product_names.push(prods[i].name + prods[i].price);
       }
     }else{
-      product_names.push(prods[i]);
+      product_names.push(prods[i].name + prods[i].price);
     }
   }
 }
@@ -146,12 +145,16 @@ product_names = product_names.sort(function (a, b) {
   return a.price - b.price;
 });
 
+<<<<<<< HEAD
 for (let i=0; i<product_names.length; i+=1){
   product_sorted_with_price.push(product_names[i].name + "  " + product_names[i].price)
 }
 
 
 return product_sorted_with_price;
+=======
+return product_names;
+>>>>>>> parent of a3bbb26... hop
 }
 
 // Calculate the total price of items, with received parameter being a list of products
