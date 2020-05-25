2c4fa4b8c3bac69dcb220c97cdb1896bbaf3a38f
jdetc021 <jdetc021@uottawa.ca>
2020-05-24 20:13:53 -0400

    push shtring

--- SEG3525-Module2/scripts/groceries.js
+++ SEG3525-Module2/scripts/groceries.js
@@ -112,12 +112,18 @@ product_names = product_names.sort(function (a, b) {
   return a.price - b.price;
 });
 
+for (let i=0; i<product_names.length; i+=1){
+  product_names.push(product_names[i].name);
+}
 
 for (let i=0; i<product_names.length; i+=1){
   product_sorted_with_price.push(product_names[i].price + " " + product_names[i].name);
 }
 
-return {product_sorted_with_price, product_names};
+return {
+  product_sorted_with_price,
+  product_names
+};
 
 }
 
