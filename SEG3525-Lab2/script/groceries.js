2e918a17df002f5074e9bf1c02e9b0fabd894491
jdetc021 <jdetc021@uottawa.ca>
2020-05-24 23:37:51 -0400

    work

--- SEG3525-Lab2/script/groceries.js
+++ SEG3525-Lab2/script/groceries.js
@@ -111,15 +111,11 @@ product_names = product_names.sort(function (a, b) {
 });
 
 for (let i=0; i<product_names.length; i+=1){
-    product_names.push(product_names[i].name);
-}
-
-for (let i=0; i<product_names.length; i+=1){
     product_sorted_with_price.push(product_names[i].price + "-" + product_names[i].name);
 }
 
 //inspired by https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
- 
+
 
  return product_sorted_with_price;
 }
--- SEG3525-Module2/scripts/groceries.js
+++ SEG3525-Module2/scripts/groceries.js
@@ -107,22 +107,12 @@ function restrictListProducts(prods, restriction) {
   }
 
   //inspired by https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-   for (let i = 0; i < product_names.length; i+=1){
-     product_sorted_with_price.push(product_names[i].name)
-   }
 
-   return product_sorted_with_price;
-  }
 
-  /*
     //inspired by https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
     product_names = product_names.sort(function (a, b) {
         return a.price - b.price;
     });
-
-    for (let i=0; i<product_names.length; i+=1){
-        product_names.push(product_names[i].name);
-    }
 
     for (let i=0; i<product_names.length; i+=1){
         product_sorted_with_price.push(product_names[i].price + "-" + product_names[i].name);
@@ -130,7 +120,7 @@ function restrictListProducts(prods, restriction) {
 
     return product_sorted_with_price;
 
-    */
+    
 }
 
 // Calculate the total price of items, with received parameter being a list of products
